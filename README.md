7a7cce8fee64eaba4540d14c81ee494a738af957
meikuam <meikuam@gmail.com>
2021-03-31 12:51:15 +0700

    fix issues

--- README.md
+++ README.md
@@ -4,8 +4,6 @@
 Tool for delete duplicates in music.yandex.ru pages.
 It can be found at chrome web store by [link](https://chrome.google.com/webstore/detail/lazyyandexmusicdevelopers/jgjbkbfgimonbbgbcinofcahnjlnicff?hl=ru).
 
-![image1](images/image1.png)
-![image2](images/image2.png)
 
 # example view
 
--- content.js
+++ content.js
@@ -1,11 +1,10 @@
-$(document).ready(function() { 
-$.get(chrome.extension.getURL('/injected.js'),
+$(document).ready(function() {
+	$.get(chrome.extension.getURL('/injected.js'),
 	function(data) {
 		var script = document.createElement("script");
 		script.setAttribute("type", "text/javascript");
 		script.innerHTML = data;
 		document.getElementsByTagName("head")[0].appendChild(script);
-		document.getElementsByTagName("body")[0].setAttribute("onLoad", "injected_main();");
 	}
 );
 });
